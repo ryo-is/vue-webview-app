da1113e2483b00db11afbe4d72cdd12dadd7eca8
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-05 14:45:04 +0900

    feat:WebView/Native間でメッセージのやり取りをする

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>{{ title }}</h1>
+    <p>{{ testText }}</p>
+    <el-button type="primary" @click="callNativeFunc()">Test</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+  public title: string = "WebView App";
+  public testText: string = "";
+
+  public mounted() {
+    const self: Home = this;
+    (window as any).testFunc = function(text: string) {
+      self.testText = text;
+    };
+  }
+
+  public destroyed() {
+    window.removeEventListener("testFunc", this.testFunc as any, false);
+  }
+
+  public callNativeFunc() {
+    alert("{key: \"Message From WebView\", value: 999}");
+  }
+
+  public testFunc(text: string) {
+    this.testText = text;
+  }
+}
 </script>

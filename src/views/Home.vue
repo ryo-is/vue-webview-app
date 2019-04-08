e645d4dd2eb6fbbffd4182b753d3299564ad7b23
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-08 14:20:17 +0900

    feat:RESTAPIのtestボタン実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,16 @@
     <h1>{{ title }}</h1>
     <p>{{ testText }}</p>
     <el-button type="primary" @click="callNativeFunc()">Test</el-button>
+    <el-button type="primary" @click="getAPITest()">Rest GET</el-button>
+    <el-button type="primary" @click="getAPITest()">Rest POST</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+
+const apiUrl: string = "https://wk38g6uoe4.execute-api.ap-northeast-1.amazonaws.com/prod/";
 
 @Component({})
 export default class Home extends Vue {
@@ -24,6 +29,22 @@ export default class Home extends Vue {
 
   public callNativeFunc() {
     alert("{key: \"Message From WebView\", value: 999}");
+  }
+
+  public async getAPITest() {
+    const result: any = await axios({
+      method: "GET",
+      url: apiUrl + "get"
+    });
+    console.log(result);
+  }
+
+  public async postAPITest() {
+    const result: any = await axios({
+      method: "POST",
+      url: apiUrl + "post"
+    });
+    console.log(result);
   }
 
   public testFunc(text: string) {

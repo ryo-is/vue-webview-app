9261a913c44029bf85e60087bb73ebaf8932fe78
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-09 14:40:59 +0900

    fix:RESTAPIの部分を修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,13 @@
     <h1>{{ title }}</h1>
     <p>{{ testText }}</p>
     <el-button type="primary" @click="callNativeFunc()">Test</el-button>
-    <el-button type="primary" @click="getAPITest()">Rest GET</el-button>
-    <el-button type="primary" @click="getAPITest()">Rest POST</el-button>
+    <el-button type="primary" @click="gqlQuery()">Query</el-button>
+    <el-button type="primary" @click="gqlMutation()">Mutation</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import axios from "axios";
-
-const apiUrl: string = "https://wk38g6uoe4.execute-api.ap-northeast-1.amazonaws.com/prod/";
 
 @Component({})
 export default class Home extends Vue {
@@ -31,20 +28,12 @@ export default class Home extends Vue {
     alert("{key: \"Message From WebView\", value: 999}");
   }
 
-  public async getAPITest() {
-    const result: any = await axios({
-      method: "GET",
-      url: apiUrl + "getReq"
-    });
-    console.log(result);
+  public gqlQuery() {
+    alert("{action: \"query\"}");
   }
 
-  public async postAPITest() {
-    const result: any = await axios({
-      method: "POST",
-      url: apiUrl + "postReq"
-    });
-    console.log(result);
+  public gqlMutation() {
+    alert("{action: \"mutation\"}");
   }
 
   public testFunc(text: string) {

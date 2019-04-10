ce5111c124f4eaef9672af674efb6ef73b3ef015
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-04-10 10:52:42 +0900

    feat:テスト用のformを作成

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,16 @@
 </template>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fcfcfc;
   margin-top: 60px;
 }
 </style>
--- src/views/home/home.ts
+++ src/views/home/home.ts
@@ -1,21 +1,16 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>{{ title }}</h1>
-    <p>{{ testText }}</p>
-    <el-button type="primary" @click="callNativeFunc()">Test</el-button>
-    <!-- <el-button type="primary" @click="gqlQuery()">Query</el-button>
-    <el-button type="primary" @click="gqlMutation()">Mutation</el-button> -->
-  </div>
-</template>
-
-<script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class Home extends Vue {
   public title: string = "WebView App";
   public testText: string = "";
+  public nickName: string = "KawasakiTaro";
+  public birthday: string = "2000/01/01";
+  public gender: string = "Male";
+  public ridingStartYear: string = "2019/04/01";
+  public area: string = "Asia/Kyoto";
+
+  public genderItems: string[] = ["Male", "Female"];
 
   public mounted() {
     const self: Home = this;
@@ -40,4 +35,3 @@ export default class Home extends Vue {
     this.testText = text;
   }
 }
-</script>

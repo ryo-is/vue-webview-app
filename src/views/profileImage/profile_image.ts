21f4fa5e621f47e4d1d5368e08d1e67f4e2948d9
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-06-10 17:05:54 +0900

    feat:画像を選択してプレビューを表示する

--- src/views/profileImage/profile_image.ts
+++ src/views/profileImage/profile_image.ts
@@ -1,4 +1,19 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
-export default class ProfileImage extends Vue {}
+export default class ProfileImage extends Vue {
+  public uploadImage: any = "";
+
+  public selectedFile(event: Event) {
+    event.preventDefault();
+    const files: any = (event.target as any).files;
+    const file: any = files[0];
+    const reader: FileReader = new FileReader();
+    reader.onload = (readerEvent: any) => {
+      this.uploadImage = readerEvent.target.result;
+    };
+    reader.readAsDataURL(file);
+    console.log(this.uploadImage);
+    console.log(file);
+  }
+}
